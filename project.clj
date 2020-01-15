711875a23cb425238f7903a3c8bfaf79b8a9a193
anekos <anekos@snca.net>
2020-01-15 21:06:23 +0900

    Rename project

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject mpd "0.1.0-SNAPSHOT"
+(defproject mpd-timer "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
